909d106d064bd75b8d51aa29113ef05db6fd0635
urlampranita <pranitaurlam@gmail.com>
2026-01-06 13:06:18 +0530

    Initial React K-pop 3rd gen gallery

---
+++ src/Card.jsx
@@ -0,0 +1,48 @@
+function Card(){
+    return(
+        <>
+        <h1 className="write">🎤Some Famous K-POP 3rd Gen groups🎶</h1>
+        <div className="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzd2l9SbrAZgxCcTtgBpj4EXTB4wlrjQo5SPH-QUU7d9LS5wfrtQQBbjtQgfrzlyBR7uGs&s"></img>
+            <h2>BTS</h2>
+            <p>It is famous international boyband from South Korea. Which comes under 3rd Gen</p>
+        </div>
+        <div className="card">
+            <img src="https://www.rollingstone.com/wp-content/uploads/2022/07/%E1%84%87%E1%85%B3%E1%86%AF%E1%84%85%E1%85%A2%E1%86%A8%E1%84%91%E1%85%B5%E1%86%BC%E1%84%8F%E1%85%B3-%E1%84%80%E1%85%A9%E1%86%BC%E1%84%89%E1%85%B5%E1%86%A8-%E1%84%8B%E1%85%B5%E1%84%86%E1%85%B5%E1%84%8C%E1%85%B5.jpg?w=1581&h=1054&crop=1" width={273} height={183}></img>
+            <h2>Black-Pink</h2>
+            <p>It is famous international girlgroup from South Korea. Which comes under 3rd Gen</p>
+        </div>
+        <div className="card">
+            <img src="https://imageio.forbes.com/specials-images/imageserve/5e15da17888ec0000690782e/0x0.jpg?format=jpg&crop=560,420,x0,y82,safe&height=900&width=1600&fit=bounds" width={273} height={183}></img>
+            <h2>Exo</h2>
+            <p>Exo is a South Korean-Chinese boy band. Which comes under 3rd Gen</p>
+        </div>
+        <div className="card">
+            <img src="https://nolae.eu/cdn/shop/articles/dies-ist-die-story-einer-girlgroup-die-unsere-herzen-beruhrt-twice-975458.jpg?v=1634896390&width=1200" width={273} height={183}></img>
+            <h2>Twice</h2>
+            <p>Twice is a South Korean girl group formed by JYP Entertainment. Which comes under 3rd Gen</p>
+        </div>
+        <div className="card">
+            <img src="https://media.allure.com/photos/5b7b0983b60c70133b1eaaf0/16:9/w_2560%2Cc_limit/Red%2520Velvet%2520Power%2520Up.jpg" width={273} height={183}></img>
+            <h2>Red-Velvet</h2>
+            <p>Red Velvet is a South Korean girl group formed and managed by SM Entertainment. Which comes under 3rd Gen</p>
+        </div>
+        <div className="card">
+            <img src="https://thebiaslist.com/wp-content/uploads/2018/07/mamamoo-egotistic.jpg" width={273} height={183}></img>
+            <h2>MAMAMOO</h2>
+            <p>Mamamoo is a South Korean girl group formed and managed by RBW. Which comes under 3rd Gen</p>
+        </div>
+        <div className="card">
+            <img src="https://www.billboard.com/wp-content/uploads/2020/02/GOT7-press-photo-2020-billboard-1548-1582816394.jpg" width={273} height={183}></img>
+            <h2>GOT7</h2>
+            <p>Got7 is a South Korean boy band formed by JYP Entertainment. Which comes under 3rd Gen</p>
+        </div>
+        <div className="card">
+            <img src="https://www.nme.com/wp-content/uploads/2021/10/seventeen-attacca-rock-with-you-pledis-221021.jpg" width={273} height={183}></img>
+            <h2>SeveenTeen</h2>
+            <p>Seventeen is a South Korean boy band formed by Pledis Entertainment. Which comes under 3rd Gen</p>
+        </div>
+        </>
+    );
+}
+export default Card
